c45b1718fd9863f8726575a1c8f24ad41b5e6e9c
yugvithani-simform <yug.vithani@simformsolutions.com>
2026-02-02 13:33:25 +0530

    multiple commit-1, done with squash
    multiple commit-2
    
    multiple commit-3
    
    multiple commit-4

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("First commit in the feat1 branch");
+console.log("Second commit in the feat1 branch");
+console.log("Third commit in the feat1 branch");
+console.log("Fourth commit in the feat1 branch");
